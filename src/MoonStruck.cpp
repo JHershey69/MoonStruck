ade5f89f08552f5263c813f8de6b9f0b0ba230c2
Jessica Hershey <60907719+JHershey69@users.noreply.github.com>
2020-12-28 15:12:41 -0500

    v1.0.0

---
+++ src/MoonStruck.cpp
@@ -0,0 +1,74 @@
+#include "MoonStruck.h"
+
+MoonStruck::MoonStruck()
+{
+  //ctor
+}
+
+
+int MoonStruck::buildURL(char* _KEY, float _LAT, float _LON)
+{
+  sprintf(moonURL, "https://api.ipgeolocation.io/astronomy?apiKey=%s&lat=%f&long=%f", _KEY, _LAT, _LON);
+
+  return 0;
+}
+
+int MoonStruck::parseMoonStruck()
+{
+  int error_code;
+
+  HTTPClient http;
+  http.begin(moonURL);
+
+  int httpCode = http.GET();
+
+  if (httpCode > 399)
+  {
+    if (httpCode == 404)
+    {
+      http.end();
+      error_code += 4;
+      return error_code;
+    }
+    else
+    {
+      http.end();
+      error_code += 5;
+      return error_code;
+    }
+
+  }
+
+  DynamicJsonDocument doc(768);
+  deserializeJson(doc, http.getString());
+
+  location_latitude = doc["location"]["latitude"]; // 39.953728
+  location_longitude = doc["location"]["longitude"]; // -74.197922
+
+  date = doc["date"]; // "2020-12-28"
+  current_time = doc["current_time"]; // "13:08:16.127"
+  sunrise = doc["sunrise"]; // "07:17"
+  sunset = doc["sunset"]; // "16:39"
+  sun_status = doc["sun_status"]; // "-"
+  solar_noon = doc["solar_noon"]; // "11:58"
+  day_length = doc["day_length"]; // "09:22"
+  sun_altitude = doc["sun_altitude"]; // 24.772448087739075
+  sun_distance = doc["sun_distance"]; // 147116479.53624782
+  sun_azimuth = doc["sun_azimuth"]; // 197.6059572088118
+  moonrise = doc["moonrise"]; // "15:29"
+  moonset = doc["moonset"]; // "05:56"
+  moon_status = doc["moon_status"]; // "-"
+  moon_altitude = doc["moon_altitude"]; // -18.923851294326536
+  moon_distance = doc["moon_distance"]; // 397569.6133900794
+  moon_azimuth = doc["moon_azimuth"]; // 33.592474665393695
+  moon_parallactic_angle = doc["moon_parallactic_angle"]; // -17.897918830310246
+
+  http.end();
+  return 0;
+}
+
+
+MoonStruck::~MoonStruck()
+{
+  //dtor
+}
---
+++ src/MoonStruck.h
@@ -0,0 +1,47 @@
+#ifndef MOONSTRUCK_H
+#define MOONSTRUCK_H
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
+
+class MoonStruck
+{
+  public:
+    MoonStruck();
+    virtual ~MoonStruck();
+    int buildURL(char* _KEY, float _LAT, float _LON);
+    int parseMoonStruck(void);
+
+    //Variables
+    float location_latitude; // 39.953728
+    float location_longitude; // -74.197922
+
+    const char* date; // "2020-12-28"
+    const char* current_time; // "13:08:16.127"
+    const char* sunrise; // "07:17"
+    const char* sunset; // "16:39"
+    const char* sun_status; // "-"
+    const char* solar_noon; // "11:58"
+    const char* day_length; // "09:22"
+    float sun_altitude; // 24.772448087739075
+    float sun_distance; // 147116479.53624782
+    float sun_azimuth; // 197.6059572088118
+    const char* moonrise; // "15:29"
+    const char* moonset; // "05:56"
+    const char* moon_status; // "-"
+    float moon_altitude; // -18.923851294326536
+    float moon_distance; // 397569.6133900794
+    float moon_azimuth; // 33.592474665393695
+    float moon_parallactic_angle; // -17.897918830310246
+
+
+  protected:
+
+  private:
+  
+char moonURL[200];
+
+};
+
+#endif // MOONSTRUCK_H
