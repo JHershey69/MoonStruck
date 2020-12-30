68e95254db8a143ec7a20c549873c01731542354
Jessica Hershey <60907719+JHershey69@users.noreply.github.com>
2020-12-30 13:52:36 -0500

    v1.0.0

---
+++ examples/simple.ino
@@ -0,0 +1,55 @@
+#include <MoonStruck.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+
+
+#define HOMESSID "*****"
+#define HOMEPW "******"
+#define MOONKEY "1a277e9ce09343f9b7e21ffeb6525c26"
+
+
+float myLATITUDE = 39.9537291;
+float myLONGITUDE = -74.1979192;
+
+
+//Invoke the library
+MoonStruck MS;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  delay(200);
+  Serial.println("Serial Initialized");
+
+  WiFi.begin(HOMESSID, HOMEPW);
+  Serial.print("Connecting...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nCONNECTED!\n");
+  }
+  
+  
+  //Call the api
+  MS.parseMoonStruck(MOONKEY, myLATITUDE, myLONGITUDE);
+
+
+
+  //Print Altitude, moonrise and moonset
+  printf("\nMoon Altitude: %f\nMoonrise: %s\nMoonset: %s\n", MS.moon_altitude, MS.moonrise, MS.moonset);
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
